3f66e853675ff17a741673291aaf8cb6a37fcb2a
Zachariarian <zachariarian@gmail.com>
2022-10-28 10:23:24 +0300

    Silly me

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
-# Sets up my web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
+# Sets up a web server for deployment of web_static.
 
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo apt-get update
+sudo apt-get install -y nginx
 
-touch /data/web_static/releases/test/index.html
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
-chown -R ubuntu:ubuntu /data/
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /etc/nginx/html;
+    index  index.html index.htm;
 
-print %s "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	server_name _;
-	add_header X-Served-By $HOSTNAME;
-	location /hbnb_static/ {
-		alias /data/web_static/current/hbnb_static/;
-		index index.html;
-	}
-}" > /etc/nginx/sites-enabled/default
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-service nginx restart
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+sudo service nginx restart
